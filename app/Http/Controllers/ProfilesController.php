d4b8f1f69bce78dbd0573d5208d56dc99575656a
dudox <snub.com@gmail.com>
2021-03-31 10:52:50 +0100

    update profile

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -4,39 +4,51 @@ namespace App\Http\Controllers;
 
 use \App\Models\User;
 use \App\Models\Profile;
-use Auth;
-
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProfilesController extends Controller
 {
 
     public function index(User $user)
     {
-        
+
         return view('profiles.index', compact('user'));
     }
 
     public function edit(User $user)
     {
-        
+
         return view('profiles.edit', compact('user'));
     }
 
     public function update(User $user)
     {
-        $data = request()->validate([
+        request()->validate([
             'title' => 'required',
             'description' => 'required',
             'url' => 'url',
             'image' => '',
         ]);
+        try {
 
-        $id = Auth::user()->id;
+            $profile = Profile::where('user_id', $user->id);
 
-        $profile = Profile::where('user_id', $id)->first();
-        $profile->update($data);
 
-        return redirect("/profile/{$user->id}");
+
+            // check if the total count of entries in the profile table is equals to zero
+            // which means user does not exists on the profile table
+            if (count($profile->get()) == 0) {
+                request()->merge(['user_id' => $user->id]); // merge user_id from user instance to request object array
+                $profile->create(request()->all()); // create a new profile account
+                return redirect()->route(['profile', $user->id]);
+            }
+            // update profile
+            $profile->update(request()->only(['title', 'description', 'url', 'image']));
+            return redirect()->back()->with(['success' => 'profile updated successfully']);
+        } catch (Exception $e) {
+            return redirect()->back()->with(['failed' => $e->getMessage()]);
+        }
     }
 }
